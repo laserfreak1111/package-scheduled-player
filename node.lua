cb462e7997f4f257374d75abc9c8873264155c20
Florian <16818714+laserfreak1111@users.noreply.github.com>
2020-12-02 21:00:35 +0100

    test für datumsanzeige

--- node.lua
+++ node.lua
@@ -1074,7 +1074,7 @@ local function TimeTile(asset, config, x1, y1, x2, y2)
 					font:write(x, y1, mydate, size, r,g,b,1)
 					
 				elseif clock_type == "weekday" then
-					days = {"Sonntag","Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"}
+					local days = {"Sonntag","Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"}
 				
 					--local mydate = os.date("Wochennummer: %w")
 					local mydate = days[0]
